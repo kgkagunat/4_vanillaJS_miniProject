74c25545a5904e64c6fc567e36536e714ec19b45
kgkagunat <kgkagunat@gmail.com>
2023-11-02 12:50:08 -1000

    added more methods, organized code

--- script.js
+++ script.js
@@ -6,64 +6,149 @@ const firstName = 'John';
 const lastName = 'Doe';
 console.log(`Hello my name is ${firstName} ${lastName}. I am ${age} years old`);
 
-// Methods / Properties
-const string = 'Hello World!';
+//=====================================================================
+console.log('--String Methods--');
 
-let result;
+//* STRING METHODS
 
-result = string;
-result = string[0];
+// 1) .at(index) - Returns character at `specified index`. Based on UTF-16 code units. `Can use` negative numbers. Returns `undefined` if index is out of range.
+const strAt = 'Hello World';
+console.log(strAt.at(4));
+console.log(strAt.at(-1));
 
-// Upper / lower case
-result = string.toUpperCase();
-result = string.toLowerCase();
+// 2 ) .charAt(index) - Returns character at `specified index`. Based on UTF-16 code units. `Can't use` negative numbers. Returns `empty string` if index is out of range.
+const strCharAt = 'Hello World';
+console.log(strCharAt.charAt(4));
+console.log(strCharAt.charAt(-1));
 
-// charAt() - return char at specified index
-result = string.charAt(9);
+// 3) .charCodeAt(index) - Returns UTF-16 code unit at `specified index`. Returns `NaN` if index is out of range.
+const strCharCodeAt = 'Hello World';
+console.log(strCharCodeAt.charCodeAt(4));
+console.log(strCharCodeAt.charCodeAt(-1));
 
-// indexOf() - return index of 1st occurrence of specified value in string
-result = string.indexOf('!');
-result = string.indexOf('W');
+// 4) .concat(str1, str2, ...) - Combines strings. Returns new string.
+const strConcat = 'Hello ';
+const strConcatResult = strConcat.concat('World', '!');
+console.log(strConcatResult);
 
-// substring(indexStart, indexEnd) - search string at specified index
-// indexStart = inclusive, indexEnd = exclusive
-result = string.substring(2);
-result = string.substring(0, 8);
+// 5) .endWith(searchString, endPosition) - Checks if string ends with `searchString`. Returns `boolean`.
+const strEndWith = 'Awesome Sauce';
+const strEndWithResult = strEndWith.endsWith('Sauce');
+console.log(strEndWithResult);
 
-// slice(indexStart, indexEnd) - slice part of string, return new string
-// indexStart = inclusive, indexEnd = exclusive
-result = string.slice(1, 5);
-result = string.slice(-6, -1);
+// 6) .includes(searchString, position) - Checks if string contains `searchString`. Returns `boolean`.
+const strIncludes = 'Awesome Sauce';
+if (strIncludes.includes('Sauce')) {
+    console.log('It does include Sauce');
+} else {
+    console.log('It does not include Sauce');
+}
 
-// trim() - remove whitespace
-result = '        Hello World!        ';
-result = result.trim();
+// 7) .indexOf(searchString, position) - Returns the index of `first` occurrence of `searchString`. Returns `-1` if not found.
+const strIndexOf = 'The World is Awesome';
+console.log(strIndexOf.indexOf('World'));
+console.log(strIndexOf.indexOf('e'));
 
-// replace(searchString, position) - replace all instances of string
-result = string.replace('Hello', 'Awesome');
+// 8) .lastIndexOf(searchString, position) - Returns the index of `last` occurrence of `searchString`. Returns `-1` if not found.
+const strLastIndexOf = 'The World is Awesome';
+console.log(strLastIndexOf.lastIndexOf('e'));
+console.log(strLastIndexOf.lastIndexOf('i'));
 
-// includes() - returns true if string found
-result = string.includes('Wor');
+// 9) .match(regexp) - Returns an array of results matching `regexp`. Returns `null` if not found.
+const strMatch = 'The World is Awesome';
+console.log(strMatch.match(/is/g));
 
-// valueOf() - returns primitive value
-result = string.valueOf();
+// 10) .padEnd(targetLength, padString) - Pads the end of the string with `padString` until it reaches `targetLength`. Returns `new` string. targetLength = string.length + padEnd(length)
+const strPadEnd = 'Peace';
+const strPadEndResult = strPadEnd.padEnd(10, '!');
+console.log(strPadEndResult);
 
-// split() - return array of strings
-result = string.split();
-result = string.split('');
-result = string.split(' ');
-result = string.split('H');
-result = string.split('!');
-result = 'People, Places, Things';
-result = result.split(',');
+// 11) .padStart(targetLength, padString) - Pads the start of the string with `padString` until it reaches `targetLength`. Returns `new` string. targetLength = string.length + padStart(length)
+const strPadStart = 'Peace';
+const strPadStartResult = strPadStart.padStart(10, '!');
+console.log(strPadStartResult);
 
-// join() - return new string by concat array elements
-result = ['The', 'Planet'];
-result = result.join(' ');
+// 12) .repeat(count) - Returns a new string containing `count` copies of the string. Returns `empty string` if count is 0.
+const strRepeat = 'Test';
+const strRepeatResult = strRepeat.repeat(3);
+console.log(strRepeatResult);
 
-console.log(result);
+// 13) .replace(pattern, replacement) - replaces pattern with replacement on `first` occurrence. Returns `new` string.
+const strReplace = 'This Word Word Cool';
+const strReplaceResult = strReplace.replace('Word', 'is');
+console.log(strReplaceResult);
 
-/* ------------------------------------ */
+// 14) .replaceAll(pattern, replacement) - replaces pattern with replacement on `all` occurrences. Returns `new` string.
+const strReplaceAll = 'This Word Word Cool';
+const strReplaceAllResult = strReplaceAll.replaceAll('Word', 'is');
+console.log(strReplaceAllResult);
+
+// 15) .search(regexp) - Returns the index of the `first` occurrence of `regexp`. Returns `-1` if not found.
+const strSearch = 'This is a search';
+const strSearchResult = strSearch.search('search');
+console.log(strSearchResult);
+
+// 16) .slice(indexStart(inclusive), indexEnd(exclusive)) - Returns a `new` string from `indexStart` to `indexEnd`. `Can use` negative numbers.
+const strSlice = 'This is a slice';
+const strSliceResult = strSlice.slice(5, 10);
+console.log(strSliceResult);
+
+// 16) .split(separator, limit) - Splits string into an array of substrings. `Can use` negative numbers. Returns `new` array.
+const strSplit = 'This is a split';
+const strSplitResult = strSplit.split(' ');
+console.log(strSplitResult);
+
+const strSplit2 = 'Here is a bunch of words';
+const strSplitResult2 = strSplit2.split(' ', 2);
+console.log(strSplitResult2);
+
+// 17) .startsWith(searchString, position) - Checks if string starts with `searchString`. Returns `boolean`.
+const strStartsWith = 'This is a startsWith';
+const strStartsWithResult = strStartsWith.startsWith('This');
+console.log(strStartsWithResult);
+
+// 18) .substring(indexStart(inclusive), indexEnd(exclusive)) - Returns a `new` string from `indexStart` to `indexEnd`. `Can't use` negative numbers.
+const strSubstring = 'This is a substring';
+const strSubstringResult = strSubstring.substring(5, 10);
+console.log(strSubstringResult);
+
+// 19) .toLowerCase() - Returns a `new` string with all lower case letters.
+const strLowerCase = 'This is a lower case';
+const strLowerCaseResult = strLowerCase.toLowerCase();
+console.log(strLowerCaseResult);
+
+// 20) .toUpperCase() - Returns a `new` string with all upper case letters.
+const strUpperCase = 'This is a upper case';
+const strUpperCaseResult = strUpperCase.toUpperCase();
+console.log(strUpperCaseResult);
+
+// 21) .toString() - Returns a `new` string.
+const strToString = 123;
+const strToStringResult = strToString.toString();
+console.log(strToStringResult);
+
+// 22) .trim() - Removes whitespace from both ends of string. Returns `new` string.
+const strTrim = '    This is a trim    ';
+const strTrimResult = strTrim.trim();
+console.log(strTrimResult);
+
+// 23) .trimEnd() - Removes whitespace from end of string. Returns `new` string.
+const strTrimEnd = '    This is a trimEnd    ';
+const strTrimEndResult = strTrimEnd.trimEnd();
+console.log(strTrimEndResult);
+
+// 24) .trimStart() - Removes whitespace from start of string. Returns `new` string.
+const strTrimStart = '    This is a trimStart    ';
+const strTrimStartResult = strTrimStart.trimStart();
+console.log(strTrimStartResult);
+
+// 25) .valueOf() - Returns `string` value of string.
+const strValueOf = 'This is a valueOf';
+const strValueOfResult = strValueOf.valueOf();
+console.log(strValueOfResult);
+
+//=====================================================================
+console.log('--Practice--');
 
 // Make 1st letter upper case
 const stringV1 = 'tester';
@@ -128,7 +213,7 @@ for (i = stringV4.length - 1; i >= 0; i--) {
 const reversedFinalString = reversedString;
 console.log(reversedFinalString);
 
-//=====================================================================
+/* ------------------------------------ */
 
 // Day 2 of practice
 
